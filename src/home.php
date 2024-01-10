dfed9eab18a75cd27e897c6cdb9ba3ad19352174
0422hana <2201369@s.asojuku.ac.jp>
2024-01-10 14:27:08 +0900

    a

---
+++ src/home.php
@@ -0,0 +1,21 @@
+<?php require 'db_connect.php' ?>
+<?php require 'header.php' ?>
+    
+<?php
+    echo '<h1>Cafe_Memory</h1>';
+    echo '<p>[カフェ一覧]</p>';
+    $pdo = new PDO($connect, USER, PASS);
+    $sql = $pdo -> prepare('select * from shop');
+    
+    echo '<table>';
+    echo '<tr><td>店舗名</td> <td>住所</td> <td>値段</td></tr>';
+    foreach($sql as $row){
+        echo '<tr>';
+        echo '<td>',$row['shop_name'],'</td>';
+        echo '<td>',$row['postcode'],$row['address'],'</td>';
+        echo '<td>',$row['price'],'</td>';
+        echo '</tr>';
+    }
+    echo '</table>';
+    ?>
+<?php require 'footer.php' ?>
