51b856df5316f5653daaf9b6e4e9f7eb4e3eb713
0422hana <2201369@s.asojuku.ac.jp>
2024-01-11 10:03:23 +0900

    Update home.php

--- src/home.php
+++ src/home.php
@@ -10,13 +10,13 @@
     $pdo = new PDO($connect, USER, PASS);
     $sql = $pdo -> query('select * from Shop');
     foreach($sql as $row){
+        $shopId = $row['shop_id'];
         echo '<tr>';
         echo '<td>',$row['shop_name'],'</td>';
         echo '<td>',$row['food_name'],'</td>';
         echo '<td>',$row['price'],'</td>';
         echo '<td>',$row['postcode'],'　　',$row['address'],'</td>';
-        //echo '<td><input type="button" name="delete" formaction="delete.php>削除</td>';
-        //echo '<td><input type="button" name="delete">削除</td>';
+        echo '<td><button onclick="location.href=`favorite.php?cosmeId=',$shopId,'`">削除</button></td>';
         echo '</tr>';
     } 
     echo '</table>';
