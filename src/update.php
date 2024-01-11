33260ca9c180723d70b00065d1b41c3717786f55
0422hana <2201369@s.asojuku.ac.jp>
2024-01-11 12:06:57 +0900

    Update update.php

--- src/update.php
+++ src/update.php
@@ -1,21 +1,22 @@
 <?php require 'db_connect.php' ?>
 <?php require 'header.php' ?>
-<script src="https://yubinbango.github.io/yubinbango/yubinbango.js" charset="UTF-8"></script>
+<!-- <script src="https://yubinbango.github.io/yubinbango/yubinbango.js" charset="UTF-8"></script> -->
 <?php
     // $pdo = new PDO($connect, USER, PASS);
     // $sql = $pdo -> prepare('delete from Shop where shop_id = ?');
     // $sql -> execute([$_GET['shopId']]);
-    echo '<form action="update-finish.php" method="post">';
-    echo '<input type="text" name="shopName">';
-    echo '<input type="text" name="foodName">';
-    echo '<input type="number" name="price">';
+    // echo '<form action="update-finish.php" method="post">';
+    // echo '<input type="text" name="shopName">';
+    // echo '<input type="text" name="foodName">';
+    // echo '<input type="number" name="price">';
     // echo '郵便番号<input type="text" id="zipCode">'
     // echo '<input type="button" id="submit" value="住所検索">';
     // echo '住所<input type="text" id="address">';
+    // echo '</form>';
 
-    echo '</form>';
-    echo '<span class="p-country-name" style="display:none;">Japan</span>';
+    echo '<script src="https://yubinbango.github.io/yubinbango/yubinbango.js" charset="UTF-8"></script>';
     echo '<form class="h-adr">';
+    echo '<span class="p-country-name" style="display:none;">Japan</span>';
     echo '〒<input type="text" class="p-postal-code" size="8" maxlength="8"><br>';
     echo '<input type="text" class="p-region p-locality p-street-address p-extended-address" />';
     echo '</form>';
