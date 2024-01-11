4eb378f9d9d2ac7d8751525ac59f70000a182f30
0422hana <2201369@s.asojuku.ac.jp>
2024-01-11 09:55:15 +0900

    Update home.php

--- src/home.php
+++ src/home.php
@@ -5,7 +5,7 @@
     echo '<h1>Cafe_Memory</h1>';
     echo '<p>[カフェ一覧]</p>';
     echo '<table>';
-    echo '<tr><td>店舗名</td> <td>住所</td> <td>値段</td></tr>';
+    echo '<tr><td>SHOP</td> <td>FOOD</td> <td>ADDRESS</td> <td></td> <td>PRICE</td></tr>';
     
     $pdo = new PDO($connect, USER, PASS);
     $sql = $pdo -> query('select * from Shop');
@@ -13,8 +13,8 @@
         echo '<tr>';
         echo '<td>',$row['shop_name'],'</td>';
         echo '<td>',$row['food_name'],'</td>';
-        echo '<td>',$row['postcode'],$row['address'],'</td>';
         echo '<td>',$row['price'],'</td>';
+        echo '<td>',$row['postcode'],'　　',$row['address'],'</td>';
         //echo '<td><input type="button" name="delete" formaction="delete.php>削除</td>';
         //echo '<td><input type="button" name="delete">削除</td>';
         echo '</tr>';
