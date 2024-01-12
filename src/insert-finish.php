577f96b62c9c4d4006aa46bdc513591582361acf
0422hana <2201369@s.asojuku.ac.jp>
2024-01-12 10:58:37 +0900

    Update insert-finish.php

--- src/insert-finish.php
+++ src/insert-finish.php
@@ -3,7 +3,7 @@
 <?php
     $pdo = new PDO($connect, USER, PASS);
     $sql = $pdo -> prepare('insert into Shop values(null, ?, ?, ?, ?, ?, ?)');
-    $sql -> execute([$_POST['shopName'], $_POST['foodName'], $_POST['category'], $_POST['price'], $_POST['zipCode'], $_POST['address']]);
+    $sql -> execute([$_POST['shopName'], $_POST['foodName'], $_POST['price'], $_POST['zipCode'], $_POST['address'], $_POST['category']]);
 ?>
 <p>登録しました</p>
 <p><a href="home.php">カフェ一覧画面へ</a></p>
