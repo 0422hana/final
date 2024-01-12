21f5309c1e8d24971f10a9b7752bf0934f32d61d
0422hana <2201369@s.asojuku.ac.jp>
2024-01-12 12:14:43 +0900

    Update home.php

--- src/home.php
+++ src/home.php
@@ -5,7 +5,7 @@
     echo '<h1>Cafe_Memory</h1>';
     echo '<div class="cafe"><p>[カフェ一覧]</p></div>';
     echo '<table border="1">';
-    echo '<tr><td width="200" height="30">SHOP</td> <td width="200" height="30">FOOD</td> <td width="110" height="30">CATEGORY</td> <td width="110" height="30">PRICE</td> <td width="250" height="30">ADDRESS</td> <td width="80" height="30"></td></tr>';
+    echo '<tr><td width="230" height="30">SHOP</td> <td width="230" height="30">FOOD</td> <td width="100" height="30">CATEGORY</td> <td width="100" height="30">PRICE</td> <td width="290" height="30">ADDRESS</td> <td width="30" height="30"></td><td width="30" height="30"></td></tr>';
     
     $pdo = new PDO($connect, USER, PASS);
     $sql = $pdo -> query('select * from Shop inner join Category on Shop.category_id = Category.category_id');
@@ -15,7 +15,7 @@
         echo '<td>',$row['shop_name'],'</td>　';
         echo '<td>',$row['food_name'],'</td>　';
         echo '<td>',$row['category_name'],'</td>　';
-        echo '<td>',$row['price'],'</td>　';
+        echo '<td>￥',$row['price'],'</td>　';
         echo '<td>',$row['postcode'],'　',$row['address'],'</td>　';
         echo '<td><button onclick="location.href=`update.php?shopId=',$shopId,'`">更新</button></td>';
         echo '<td><button onclick="location.href=`delete.php?shopId=',$shopId,'`">削除</button></td>';
