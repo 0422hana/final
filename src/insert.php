c6630cb855abbce0ab0bad5608752715eeea1dbc
0422hana <2201369@s.asojuku.ac.jp>
2024-01-12 11:17:11 +0900

    Update insert.php

--- src/insert.php
+++ src/insert.php
@@ -5,14 +5,14 @@
     // $sql = $pdo -> prepare('delete from Shop where shop_id = ?');
     // $sql -> execute([$_GET['shopId']]);
     echo '<form action="insert-finish.php" method="post" class="h-adr">';
-    echo 'SHOP NAME<input type="text" name="shopName"><br>';
-    echo 'FOOD NAME<input type="text" name="foodName"><br>';
-    echo '<select name="category">';
+    echo 'SHOP NAME：<input type="text" name="shopName"><br>';
+    echo 'FOOD NAME：<input type="text" name="foodName"><br>';
+    echo 'FOOD CATEGORY：<select name="category">';
     echo    '<option value="1">スイーツ</option>';
     echo    '<option value="2">ランチ</option>';
     echo    '<option value="3">ディナー</option>';
     echo '</select><br>';
-    echo 'PRICE<input type="number" name="price"><br>';
+    echo 'PRICE：<input type="number" name="price"><br>';
     echo '<script src="https://yubinbango.github.io/yubinbango/yubinbango.js" charset="UTF-8"></script>';
     // echo '<form class="h-adr">';
     echo '<span class="p-country-name" style="display:none;">Japan</span>';
