c9c56062e035c5f05bb741647da5b430d26325e4
0422hana <2201369@s.asojuku.ac.jp>
2024-01-15 13:42:25 +0900

    Update file_finish.php

--- src/file_finish.php
+++ src/file_finish.php
@@ -5,9 +5,11 @@ $image = $_FILES['image'];
         if($image['size'] <= 5000000){
             $filename = uniqid().'.'.pathinfo($image['name'], PATHINFO_EXTENSION);
             $destination = '../image/'.$filename;
+            $db_fileName = 'image/'.$filename;
             if(move_uploaded_file($image['tmp_name'], $destination)){
                 echo 'ファイルが正常にアップロードされました。';
                 echo $destination;
+                echo $db_fileName;
                 echo '<img src="'.$destination.'" alt="アップロードされた画像">';
             }else{
                 echo 'ファイルのアップロードに失敗しました。';
